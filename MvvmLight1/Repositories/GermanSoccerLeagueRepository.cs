2068c110a81f18831822bc01fecee242cc986e0e
Andre <andre@langen-brueggen.de>
2018-09-23 14:07:13 +0200

    Added use of rest api. (To be continued...)

--- MvvmLight1/Repositories/GermanSoccerLeagueRepository.cs
+++ MvvmLight1/Repositories/GermanSoccerLeagueRepository.cs
@@ -1,8 +1,11 @@
 ﻿using MvvmLight1.Interfaces;
 using MvvmLight1.Model;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,20 +15,55 @@ namespace MvvmLight1.Repositories
     {
         public List<SoccerMatch> GetSoccerMatches()
         {
-            var result = new List<SoccerMatch>
+            var result = new List<SoccerMatch>();
+
+            using (HttpClient client = new HttpClient())
             {
-                new SoccerMatch
+                client.BaseAddress = new Uri("https://www.openligadb.de/api/getmatchdata/bl1");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = client.GetAsync("").Result;
+                if (response.IsSuccessStatusCode)
                 {
-                    Id = 1,
-                    LeagueName = "1. Fußball-Bundesliga 2018/2019",
-                    StartDate = new DateTime(2018, 9, 21),
-                    IsMatchFinished = true,
-                    Team1 = new SoccerTeam{ Id = 1, Name = "VfB Stuttgart", ShortName = "Stuttgart" },
-                    Team2 = new SoccerTeam{ Id = 2, Name = "Fortuna Düsseldorf", ShortName = "Düsseldorf" },
+                    var dataObjects = response.Content.ReadAsAsync<IEnumerable<JObject>>().Result;
+                    result = Convert(dataObjects).ToList();
                 }
-            };
+                else
+                {
+                    int foo = 1;
+                }
+            }
 
             return result;
+        }
+
+        private IEnumerable<SoccerMatch> Convert(IEnumerable<JObject> jsonMatchObjects)
+        {
+            return jsonMatchObjects.Select(NewSoccerMatchByJson);
+        }
+        private SoccerMatch NewSoccerMatchByJson(dynamic jsonObject)
+        {
+            return new SoccerMatch
+            {
+                Id = jsonObject.MatchID,
+                LeagueName = jsonObject.LeagueName,
+                StartDate = jsonObject.MatchDateTime,
+                IsMatchFinished = jsonObject.MatchIsFinished,
+                Team1 = NewTeamByJson(jsonObject.Team1),
+                Team2 = NewTeamByJson(jsonObject.Team2)
+            };
+        }
+        private SoccerTeam NewTeamByJson(dynamic jsonObject)
+        {
+            string url = jsonObject.TeamIconUrl;
+
+            return new SoccerTeam
+            {
+                Id = jsonObject.TeamId,
+                Name = jsonObject.TeamName,
+                ShortName = jsonObject.ShortName,
+                IconUrl = new Uri(url)
+            };
         }
     }
 }
