a055c0cba35e1af5f9256071d649383380673b9c
Andre <andre@langen-brueggen.de>
2018-09-29 19:51:01 +0200

    Added class SoccerTableEntry.

---
+++ MvvmLight1/Model/SoccerTableEntry.cs
@@ -0,0 +1,15 @@
+﻿namespace MvvmLight1.Model
+{
+    public class SoccerTableEntry
+    {
+        public SoccerTeam Team { get; set; }
+
+        public int NumberOfMatches { get; set; }
+        public int NumberOfPoints { get; set; }
+        public int NumberOfWonMatches { get; set; }
+        public int NumberOfLostMatches { get; set; }
+        public int NumberOfDrawMatches { get; set; }
+        public int NumberOfGoals { get; set; }
+        public int NumberOfOpponentGoals { get; set; }
+    }
+}
