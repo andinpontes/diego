666a3dbde0f35f05fa4682a7f8b640f0cf6ecfb4
Andre <andre@langen-brueggen.de>
2018-10-01 21:31:07 +0200

    Added special loading of teams.

--- Diego/Repositories/GermanSoccerLeagueRepository.cs
+++ Diego/Repositories/GermanSoccerLeagueRepository.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace Diego.Repositories
 {
@@ -15,8 +16,10 @@ namespace Diego.Repositories
         private readonly string REQUEST_URL_FOR_CURRENT_MATCHDAY = "https://www.openligadb.de/api/getmatchdata/bl1";
         private readonly string REQUEST_URL_FOR_MATCHDAY_BY_YEAR_AND_NUMBER = "https://www.openligadb.de/api/getmatchdata/bl1/{0}/{1}";
         private readonly string REQUEST_URL_FOR_TABLE_BY_YEAR = "https://www.openligadb.de/api/getbltable/bl1/{0}";
+        private readonly string REQUEST_URL_FOR_TEAMS_BY_YEAR = "https://www.openligadb.de/api/getavailableteams/bl1/{0}";
 
         private HttpClient _httpClient = new HttpClient();
+        private List<SoccerTeam> _teams = new List<SoccerTeam>();
 
         public GermanSoccerLeagueRepository()
         {
@@ -42,6 +45,8 @@ namespace Diego.Repositories
                 throw new ArgumentOutOfRangeException(nameof(number));
             }
 
+            await ValidateSoccerTeamsAsync();
+
             var dataObjects = await ReadSoccerMatchDataAsync(number);
 
             return new SoccerMatchDay
@@ -53,6 +58,8 @@ namespace Diego.Repositories
         }
         public async Task<SoccerTable> GetSoccerTable()
         {
+            await ValidateSoccerTeamsAsync();
+
             var dataObjects = await ReadSoccerTableAsync();
 
             return new SoccerTable
@@ -66,19 +73,43 @@ namespace Diego.Repositories
             _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
+        private async Task ValidateSoccerTeamsAsync()
+        {
+            if (_teams.Count > 0)
+            {
+                return;
+            }
+
+            var dataObjects = await ReadSoccerTeamsAsync();
+
+            //TODO: Add Image of team!
+            //var urlString = ((dynamic)dataObjects.First()).TeamIconUrl.ToString();
+            //var url = new Uri(urlString);
+            //var image = await LoadImageAsync(url);
+
+            _teams = dataObjects.Select(NewTeamByJson).ToList();
+        }
+        private SoccerTeam NewTeamByJson(dynamic jsonObject)
+        {
+            return new SoccerTeam
+            {
+                Id = jsonObject.TeamId,
+                Name = jsonObject.TeamName,
+                ShortName = jsonObject.ShortName,
+                IconUrl = jsonObject.TeamIconUrl
+            };
+        }
+
+        private async Task<IEnumerable<JObject>> ReadSoccerTeamsAsync()
+        {
+            var url = GetTeamsUrl();
+            return await ReadJsonDataFromUrlAsync(url);
+        }
+
         private async Task<IEnumerable<JObject>> ReadSoccerMatchDataAsync(int number)
         {
             var url = GetMatchDayUrlByNumber(number);
-
-            var response = await _httpClient.GetAsync(url);
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new HttpRequestException("Failed to read soccer data.");
-            }
-
-            //await Task.Delay(3000);
-
-            return await response.Content.ReadAsAsync<IEnumerable<JObject>>();
+            return await ReadJsonDataFromUrlAsync(url);
         }
         private string ReadLeagueName(IEnumerable<JObject> jsonMatchObjects)
         {
@@ -116,26 +147,26 @@ namespace Diego.Repositories
         }
         private SoccerMatch NewSoccerMatchByJson(dynamic jsonObject)
         {
+            var team1 = GetTeamByJsonTeamObject(jsonObject.Team1);
+
             return new SoccerMatch
             {
                 Id = jsonObject.MatchID,
                 UtcStartDate = jsonObject.MatchDateTimeUTC,
-                Team1 = NewTeamByJson(jsonObject.Team1),
-                Team2 = NewTeamByJson(jsonObject.Team2),
+                Team1 = GetTeamByJsonTeamObject(jsonObject.Team1),
+                Team2 = GetTeamByJsonTeamObject(jsonObject.Team2),
                 FinalResult = NewMatchResultByJson(jsonObject.MatchResults, "Endergebnis"),
                 HalfTimeResult = NewMatchResultByJson(jsonObject.MatchResults, "Halbzeit"),
                 IsMatchFinished = jsonObject.MatchIsFinished
             };
         }
-        private SoccerTeam NewTeamByJson(dynamic jsonObject)
+        private SoccerTeam GetTeamByJsonTeamObject(dynamic jsonTeamObject)
         {
-            return new SoccerTeam
-            {
-                Id = jsonObject.TeamId,
-                Name = jsonObject.TeamName,
-                ShortName = jsonObject.ShortName,
-                IconUrl = jsonObject.TeamIconUrl
-            };
+            return GetTeamById((int)jsonTeamObject.TeamId);
+        }
+        private SoccerTeam GetTeamById(int id)
+        {
+            return _teams.First(e => e.Id == id);
         }
         private SoccerMatchResult NewMatchResultByJson(dynamic jsonArray, string name)
         {
@@ -163,14 +194,7 @@ namespace Diego.Repositories
         private async Task<IEnumerable<JObject>> ReadSoccerTableAsync()
         {
             var url = GetTableUrl();
-
-            var response = await _httpClient.GetAsync(url);
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new HttpRequestException("Failed to read soccer table.");
-            }
-
-            return await response.Content.ReadAsAsync<IEnumerable<JObject>>();
+            return await ReadJsonDataFromUrlAsync(url);
         }
         private IEnumerable<SoccerTableEntry> ReadSoccerTableEntries(IEnumerable<JObject> jsonTableObjects)
         {
@@ -197,6 +221,11 @@ namespace Diego.Repositories
             };
         }
 
+        private string GetTeamsUrl()
+        {
+            int year = GetCurrentSeasonYear();
+            return string.Format(REQUEST_URL_FOR_TEAMS_BY_YEAR, year);
+        }
         private string GetMatchDayUrlByNumber(int number)
         {
             if (number <= 0)
@@ -218,12 +247,38 @@ namespace Diego.Repositories
             return 2018;
         }
 
+        private async Task<IEnumerable<JObject>> ReadJsonDataFromUrlAsync(string url)
+        {
+            var response = await _httpClient.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException($"Failed to read data from {url}.");
+            }
+
+            return await response.Content.ReadAsAsync<IEnumerable<JObject>>();
+        }
+
         protected virtual void Dispose(bool disposing)
         {
             if (disposing)
             {
                 _httpClient.Dispose();
             }
+        }
+
+        public async Task<BitmapImage> LoadImageAsync(Uri url)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+
+            using (var response = await _httpClient.GetAsync(url))
+            {
+                response.EnsureSuccessStatusCode();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = await response.Content.ReadAsStreamAsync();
+                bitmapImage.EndInit();
+             }
+
+             return bitmapImage;
         }
     }
 }
